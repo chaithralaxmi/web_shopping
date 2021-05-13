09104897f49514b231933638e2f50e5350e98938
Jinto Jose <jintoppy@gmail.com>
2021-05-13 21:07:29 +0530

    adding image

--- src/js/App/App.js
+++ src/js/App/App.js
@@ -1,9 +1,10 @@
-import axios from 'axios';
 import './App.scss';
+import happyFaceImg from '../../images/happy-face.jpg';
 
 class App {
   headerEl = document.createElement('h1');
   appContainerEl = document.createElement('div');
+  imgEl = document.createElement('img');
 
   getRepos = async () => {
     const res = await fetch('https://api.github.com/users');
@@ -15,11 +16,14 @@ class App {
     this.headerEl.setAttribute('id', 'myheader');
     this.appContainerEl.setAttribute('id', 'app');
     this.headerEl.textContent = 'Hi everyone';
+    this.imgEl.src = happyFaceImg;
+    this.imgEl.setAttribute('width', '150');
     this.getRepos();
   }
 
   render() {
     this.appContainerEl.append(this.headerEl);
+    this.appContainerEl.append(this.imgEl);
     return this.appContainerEl;
   }
 }
