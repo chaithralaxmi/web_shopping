ab6155636fff12cea5c1ba73b8906d60cd2580fe
Jinto Jose <jintoppy@gmail.com>
2021-05-13 20:32:01 +0530

    adding browserlist

--- src/js/App/App.js
+++ src/js/App/App.js
@@ -1,13 +1,21 @@
+import axios from 'axios';
 import './App.scss';
 
 class App {
   headerEl = document.createElement('h1');
   appContainerEl = document.createElement('div');
 
+  getRepos = async () => {
+    const res = await fetch('https://api.github.com/users');
+    const data = await res.json();
+    console.log(data);
+  };
+
   constructor() {
     this.headerEl.setAttribute('id', 'myheader');
     this.appContainerEl.setAttribute('id', 'app');
     this.headerEl.textContent = 'Hi everyone';
+    this.getRepos();
   }
 
   render() {
